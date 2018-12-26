62701ac94111b0382e6bd663ca8e722bdb754539
sejinahn2 <43711842+sejinahn2@users.noreply.github.com>
2018-12-25 18:47:14 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,6 @@ using Distributions, Parameters, LinearAlgebra, Compat, Test
         a=[1 1 1; 2 2 2; 3 3 3]
         b=rand(3,3)
         @test FixedLaspeyres(a,b) == ones(3)
+        @test FixedPaasche(a,b) == ones(3)
+        @test FixedFisher(a,b) == ones(3)
+        @test FixedTornqvist(a,b) == ones(3)
