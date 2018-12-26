edd111a815a7be4cdc9cc4204bbf8d8b784b27c4
sejinahn2 <43711842+sejinahn2@users.noreply.github.com>
2018-12-25 16:43:52 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using pindex
 using Test
 
-@testset "pindex.jl" begin
     # T3: Identity or Constant test P(p,p,q^0,q^1)=1
     a=[1 2 3; 1 2 3; 1 2 3] b=rand(3,3)
     @test FixedLaspeyres(a,b)==1
