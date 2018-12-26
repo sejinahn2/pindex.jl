82f66dabf0922f9091e9f235d9c1aad2b926f4e6
sejinahn2 <43711842+sejinahn2@users.noreply.github.com>
2018-12-25 18:38:09 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,8 @@
 using pindex
 using Distributions, Parameters, LinearAlgebra, Compat, Test
+
+
+    # T3: Identity or Constant test P(p,p,q^0,q^1)=1
+        a=[1 1 1; 2 2 2; 3 3 3]
+        b=rand(3,3)
+        @test FixedLaspeyres(a,b) == ones(3)
