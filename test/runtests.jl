7d0343a553a09add29c92e78db87311c5ab1fda5
sejinahn2 <43711842+sejinahn2@users.noreply.github.com>
2018-12-25 19:22:25 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using pindex
 using Distributions, Parameters, LinearAlgebra, Compat, Test
 
     #Tests are referred from Professor Erwin Diewerts 580 Lecture note 3
-    # T3: Identity or Constant test P(p,p,q^0,q^1)=1
+    # T3: Identity or Constant test: P(p,p,q0,q1)=1
         a=[1 1 1; 2 2 2; 3 3 3]
         b=rand(3,3)
         @test FixedLaspeyres(a,b) == ones(3)
@@ -10,4 +10,11 @@ using Distributions, Parameters, LinearAlgebra, Compat, Test
         @test FixedFisher(a,b) == ones(3)
         @test FixedTornqvist(a,b) == ones(3)
 
-    #T4: Fixed Basket or Constant Quantities Test 
+    #T5: Propotionality in Current Prices: P(p0,k*p1,q0,q1)=k*P(p0,p1,q0,q1)
+        a=rand(3,3)
+        b=rand(3,3)
+        k=5
+        c=[a[:,1] k*a[:,2] k*a[:,3]]
+        @test FixedLaspeyres(a,b)[2]*k ≈ FixedLaspeyres(c,b)[2]
+        @test FixedPaasche(a,b)[2]*k ≈ FixedPaasche(c,b)[2]
+        @test FixedFisher(a,b)[2]*k ≈ FixedFisher(c,b)[2]
