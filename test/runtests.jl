436de04611c9e3e563c3957108301c58e19a67bc
sejinahn2 <43711842+sejinahn2@users.noreply.github.com>
2018-12-25 20:15:45 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,12 @@ using Distributions, Parameters, LinearAlgebra, Compat, Test
     # T3: Identity or Constant test: P(p,p,q0,q1)=1
         a=[1 1 1; 2 2 2; 3 3 3]
         b=rand(3,3)
+        @testset "T3" begin
         @test FixedLaspeyres(a,b) ≈ ones(3)
         @test FixedPaasche(a,b) ≈ ones(3)
         @test FixedFisher(a,b) ≈ ones(3)
         @test FixedTornqvist(a,b) ≈ ones(3)
+        end 
 
     #T5: Propotionality in Current Prices: P(p0,k*p1,q0,q1)=k*P(p0,p1,q0,q1)
         a=rand(3,3)
@@ -21,9 +23,13 @@ using Distributions, Parameters, LinearAlgebra, Compat, Test
         #Note that Fixed Tornqvist does not satisfies T5
 
     #T9 Commodity Reversal Test
-    #T16 Paasche and Laspeyres Bounding Test
-
-    #Circularity Test
-        #a=rand(3,10)
-        #b=rand(3,10)
-        #@test FixedFisher(a,b)=ChainedFisher(a,b)
+    #T16 Paasche and Laspeyres Bounding Test: The price index P lies between the
+    #Laspeyers and Paashce indices
+        @test FixedFisher(a,b) < max(FixedLaspeyres(a,b),FixedPaasche(a,b))
+        @test FixedFisher(a,b) > min(FixedLaspeyres(a,b),FixedPaasche(a,b))
+        @test FixedTornqvist(a,b) < max(FixedLaspeyres(a,b),FixedPaasche(a,b))
+        @test FixedTornqvist(a,b) > min(FixedLaspeyres(a,b),FixedPaasche(a,b))
+        @test ChainedFisher(a,b) < max(ChainedLaspeyres(a,b),ChainedPaasche(a,b))
+        @test ChainedFisher(a,b) > min(ChainedLaspeyres(a,b),ChainedPaasche(a,b))
+        @test ChainedTornqvist(a,b) < max(ChainedLaspeyres(a,b),ChainedPaasche(a,b))
+        @test ChainedTornqvist(a,b) > min(ChainedLaspeyres(a,b),ChainedPaasche(a,b))
